84f24005561cbfcdde730a3a607648bcfad2d590
Rakibul-Islam01 <iamrakibulislam01@gmail.com>
2024-03-19 15:00:41 +0600

    data load from fake data

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h3 className='bg-slate-500 py-2 text-2xl'>Knowledge Cafe</h3>
+        </div>
+    );
+};
+
+export default Header;
